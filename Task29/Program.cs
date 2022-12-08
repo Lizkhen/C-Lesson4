30a0baf0af77f253e622e0e5a33b8fba21870992
Lizkhen <slezadrakona@gmail.com>
2022-12-08 10:52:14 +0300

    Task29

---
+++ Task29/Program.cs
@@ -0,0 +1,16 @@
+﻿/* Задача 29: Напишите программу, которая задаёт массив из 
+8 элементов и выводит их на экран.   */
+
+Random random = new Random();
+Console.WriteLine (ResultArray(random));
+
+string ResultArray (Random random)
+{
+    int [] array = new int [8];
+    for (int i = 0; i < array.Length ; i++)
+    {
+        array [i] = random.Next (-50,50);
+    }
+    string result;
+    return result = string.Join(" ",array);
+}
